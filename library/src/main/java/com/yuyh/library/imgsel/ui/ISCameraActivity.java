dadadd053ee8313b8322b9bc2afef7bbadad0159
LITAONO1 <646917928@qq.com>
2018-03-15 16:51:21 +0800

    修改拍照不裁剪闪退错误

--- library/src/main/java/com/yuyh/library/imgsel/ui/ISCameraActivity.java
+++ library/src/main/java/com/yuyh/library/imgsel/ui/ISCameraActivity.java
@@ -69,7 +69,7 @@ public class ISCameraActivity extends AppCompatActivity {
     }
 
     private void camera() {
-
+        cropImageFile = new File(FileUtils.createRootPath(this) + "/" + System.currentTimeMillis() + ".jpg");
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED
                 || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -102,7 +102,7 @@ public class ISCameraActivity extends AppCompatActivity {
     }
 
     private void crop(String imagePath) {
-        cropImageFile = new File(FileUtils.createRootPath(this) + "/" + System.currentTimeMillis() + ".jpg");
+        // cropImageFile = new File(FileUtils.createRootPath(this) + "/" + System.currentTimeMillis() + ".jpg");
 
         Intent intent = new Intent("com.android.camera.action.CROP");
         intent.setDataAndType(getImageContentUri(new File(imagePath)), "image/*");
